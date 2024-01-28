2696b4d1176aca8844b010bd8155e8f110152249
Sergey Mokhov <foxleren@gmail.com>
2024-01-28 07:22:15 +0300

    refactor go logger, init sage with config

--- xor-go/go.mod
+++ xor-go/go.mod
@@ -17,6 +17,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.10.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
@@ -34,6 +35,8 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -61,4 +64,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ xor-go/libs/xor_logger/logger.go
@@ -0,0 +1,57 @@
+package xor_logger
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// TODO support using sentry
+
+func InitLogger(cfg *Config) (*zap.Logger, error) {
+	env, err := ParseEnvironment(cfg.Environment)
+	if err != nil {
+		return nil, err
+	}
+	configZap, err := GetLoggerConfig(env)
+	if err != nil {
+		return nil, err
+	}
+
+	var level zapcore.Level
+	err = level.UnmarshalText([]byte(cfg.Level))
+	if err != nil {
+		return nil, fmt.Errorf("cannot unmarshall zap xor_logger level: %v", err)
+	}
+
+	// TODO support all options from original zap.Config
+	configZap.Level = zap.NewAtomicLevelAt(level)
+	configZap.OutputPaths = cfg.OutputPaths
+	configZap.ErrorOutputPaths = cfg.ErrorOutputPaths
+	configZap.Encoding = cfg.Encoding
+	fmt.Println(cfg.ErrorOutputPaths)
+
+	logger, err := configZap.Build()
+	if err != nil {
+		return nil, fmt.Errorf("zap xor_logger build failed: %v", err)
+	}
+
+	return logger, nil
+}
+
+func GetLoggerConfig(env Environment) (*zap.Config, error) {
+	var config zap.Config
+	var err error = nil
+	switch env {
+	case DevEnvironment:
+		config = zap.NewDevelopmentConfig()
+	case ProdEnvironment:
+		config = zap.NewProductionConfig()
+	default:
+		err = fmt.Errorf("cannot get xor_logger config for env: %s", env)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
