335d74d24ded99898ba27860c15adc912e0477a6
Sergey Mokhov <foxleren@gmail.com>
2024-01-28 03:29:23 +0300

    init sage

--- xor-go/services/sage/cmd/main.go
+++ xor-go/services/sage/cmd/main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	//"context"
-	"fmt"
-	"github.com/joho/godotenv"
-	"log"
-	"os"
-)
-
-const MainEnvName = ".env"
-const AppCapsName = "DRIVER"
-
-func init() {
-	if err := godotenv.Load(MainEnvName); err != nil {
-		log.Print(fmt.Sprintf("No '%s' file found", MainEnvName))
-	}
-}
+import "fmt"
 
 func main() {
-	//ctx := context.Background()
-
-	configPath := os.Getenv("CONFIG_" + AppCapsName)
-	log.Println("Driver config path: ", configPath)
-
-	// Собираем конфиг приложения
-	//cfg, err := config.NewConfig(configPath, AppCapsName)
-	//if err != nil {
-	//	log.Fatal("Fail to parse driver config: ", err)
-	//}
-	//
-	//// Создаем наше приложение
-	//application, err := app.NewApp(cfg)
-	//if err != nil {
-	//	log.Fatal(fmt.Sprintf("Fail to create %s app: %s", cfg.App.Name, err))
-	//}
-	//
-	//// Запускаем приложение
-	//application.Start(ctx)
+	fmt.Println("Sage service")
 }
