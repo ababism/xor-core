b9b75a75e24cc6a13f195404f3d1060d35293ac1
Sergey Mokhov <foxleren@gmail.com>
2024-03-19 11:32:36 +0300

    remove old idm

--- xor-java/idm/src/db-migration/V1_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE account
-(
-    id            uuid               NOT NULL,
-    login         varchar            NOT NULL UNIQUE,
-    password_hash varchar            NOT NULL,
-    contacts      jsonb              NOT NULL,
-    deleted       bool DEFAULT false NOT NULL,
-    PRIMARY KEY (id)
-)
