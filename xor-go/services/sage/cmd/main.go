d2dc8b0ac361a46e52a5d25e983d852092a27814
Sergey Mokhov <foxleren@gmail.com>
2024-04-10 02:00:43 +0300

    add go ci

--- xor-go/services/sage/cmd/main.go
+++ xor-go/services/sage/cmd/main.go
@@ -1,34 +1,23 @@
 package main
 
-import (
-	"flag"
-	"github.com/ilyakaznacheev/cleanenv"
-	"log"
-	"os"
-	"xor-go/services/sage/internal/app"
-	"xor-go/services/sage/internal/config"
-)
+import "fmt"
+
+const configPath = "./services/sage/configs/config.dev.yaml"
 
 func main() {
-	var configPath string
-	flag.StringVar(&configPath, "config-path", "", "path to .yaml config")
-	flag.Parse()
-
-	if configPath == "" {
-		path := os.Getenv("CONFIG_PATH")
-		if path == "" {
-			log.Fatal("no config file provided")
-		}
-		configPath = path
-	}
-	var appConfig config.Config
-	if err := cleanenv.ReadConfig(configPath, &appConfig); err != nil {
-		log.Fatalf("failed to read config from path: %s, error: %v", configPath, err)
-	}
-
-	application, err := app.NewApp(&appConfig)
-	if err != nil {
-		log.Fatalf("failed to create app: %v", err)
-	}
-	application.Start()
+	fmt.Println("ok")
+	//cfg, err := config.NewConfig(configPath, ServiceCapsName)
+	//if err != nil {
+	//	log.Fatalf("Fail to parse %s config: %v", ServiceName, err)
+	//}
+	//var appConfig config.Config
+	//if err := cleanenv.ReadConfig(configPath, &appConfig); err != nil {
+	//	log.Fatalf("failed to read config from path: %s, error: %v", configPath, err)
+	//}
+	//
+	//application, err := app.NewApp(&appConfig)
+	//if err != nil {
+	//	log.Fatalf("failed to create app: %v", err)
+	//}
+	//application.Start()
 }
