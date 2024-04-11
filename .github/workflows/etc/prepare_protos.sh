8008d68b63917385a14fe9162b36d92714f629ca
Sergey Mokhov <foxleren@gmail.com>
2024-04-11 08:07:54 +0300

    add Dockerfile

--- .github/workflows/etc/prepare_protos.sh
+++ .github/workflows/etc/prepare_protos.sh
@@ -6,4 +6,4 @@ export PATH=$PATH:$GOPATH/bin
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
-python3 ./xor-python/scripts/prepare_protos.py --xor-go
+python3 ./xor-python/scripts/prepare_protos.py --xor-go --xor-java
