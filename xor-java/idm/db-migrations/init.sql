d21157c4ffe4fb81ef7c2c5e96d7e3a36211a553
Sergey Mokhov <foxleren@gmail.com>
2024-04-11 20:27:58 +0300

    remove bot migrations from idm

--- xor-java/idm/db-migrations/init.sql
+++ xor-java/idm/db-migrations/init.sql
@@ -30,32 +30,3 @@ CREATE TABLE account_role
     FOREIGN KEY (account_uuid) REFERENCES account (uuid),
     FOREIGN KEY (role_uuid) REFERENCES role (uuid)
 );
-
-CREATE TABLE telegram_user
-(
-    account_uuid uuid    NOT NULL UNIQUE,
-    id           bigint  NOT NULL UNIQUE,
-    username     varchar NOT NULL UNIQUE,
-    first_name   varchar NOT NULL,
-    last_name    varchar NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE telegram_chat
-(
-    id          bigint  NOT NULL UNIQUE,
-    title       varchar NOT NULL,
-    owner_id    bigint  NOT NULL,
-    invite_link varchar,
-    PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES telegram_user (id)
-);
-
-CREATE TABLE telegram_chat_user
-(
-    chat_id bigint NOT NULL,
-    user_id bigint NOT NULL,
-    PRIMARY KEY (chat_id, user_id),
-    FOREIGN KEY (chat_id) REFERENCES telegram_chat (id),
-    FOREIGN KEY (user_id) REFERENCES telegram_user (id)
-);
